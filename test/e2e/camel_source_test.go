59511b3b2aa18b01b71b627e71fd1287f0bffff0
Nicola Ferraro <ni.ferraro@gmail.com>
2020-10-01 11:09:30 +0200

    Upgrade and fix e2e tests (#11)

--- test/e2e/camel_source_test.go
+++ test/e2e/camel_source_test.go
@@ -19,6 +19,7 @@ limitations under the License.
 package e2e
 
 import (
+	"context"
 	"os"
 	"strings"
 	"testing"
@@ -45,19 +46,22 @@ func TestCamelSource(t *testing.T) {
 		body            = "Hello, world!"
 	)
 
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	client := testlib.Setup(t, true)
 	defer testlib.TearDown(client)
 
 	t.Logf("Creating event record")
-	eventTracker, _ := recordevents.StartEventRecordOrFail(client, loggerPodName)
+	eventTracker, _ := recordevents.StartEventRecordOrFail(ctx, client, loggerPodName)
 
 	camelClient := getCamelKClient(client)
 
 	t.Logf("Creating Camel K IntegrationPlatform")
-	createCamelPlatformOrFail(client, camelClient, camelSourceName)
+	createCamelPlatformOrFail(ctx, client, camelClient, camelSourceName)
 
 	t.Logf("Creating CamelSource")
-	createCamelSourceOrFail(client, &v1alpha1.CamelSource{
+	createCamelSourceOrFail(ctx, client, &v1alpha1.CamelSource{
 		ObjectMeta: meta.ObjectMeta{
 			Name: camelSourceName,
 		},
@@ -89,12 +93,12 @@ func TestCamelSource(t *testing.T) {
 	})
 
 	t.Logf("Waiting for all resources ready")
-	client.WaitForAllTestResourcesReadyOrFail()
+	client.WaitForAllTestResourcesReadyOrFail(ctx)
 
 	t.Logf("Sleeping for 3s to let the timer tick at least once")
 	time.Sleep(3 * time.Second)
 
-	pods, err := client.Kube.Kube.CoreV1().Pods(client.Namespace).List(meta.ListOptions{
+	pods, err := client.Kube.Kube.CoreV1().Pods(client.Namespace).List(ctx, meta.ListOptions{
 		LabelSelector: "camel.apache.org/integration",
 	})
 	if err != nil {
@@ -103,7 +107,7 @@ func TestCamelSource(t *testing.T) {
 	if len(pods.Items) == 0 {
 		t.Fatalf("no integration pod found")
 	}
-	printPodLogs(t, client, pods.Items[0].Name, "integration")
+	printPodLogs(ctx, t, client, pods.Items[0].Name, "integration")
 
 	eventTracker.AssertAtLeast(1, recordevents.MatchEvent(test.AllOf(
 		test.HasData([]byte(body))),
@@ -111,29 +115,29 @@ func TestCamelSource(t *testing.T) {
 	))
 }
 
-func printPodLogs(t *testing.T, c *testlib.Client, podName, containerName string) {
-	logs, err := c.Kube.PodLogs(podName, containerName, c.Namespace)
+func printPodLogs(ctx context.Context, t *testing.T, c *testlib.Client, podName, containerName string) {
+	logs, err := c.Kube.PodLogs(ctx, podName, containerName, c.Namespace)
 	if err == nil {
 		t.Log(string(logs))
 	}
 	t.Logf("End of pod %s logs", podName)
 }
 
-func createCamelSourceOrFail(c *testlib.Client, camelSource *v1alpha1.CamelSource) {
+func createCamelSourceOrFail(ctx context.Context, c *testlib.Client, camelSource *v1alpha1.CamelSource) {
 	camelSourceClientSet, err := camelsourceclient.NewForConfig(c.Config)
 	if err != nil {
 		c.T.Fatalf("Failed to create CamelSource client: %v", err)
 	}
 
 	cSources := camelSourceClientSet.SourcesV1alpha1().CamelSources(c.Namespace)
-	if createdCamelSource, err := cSources.Create(camelSource); err != nil {
+	if createdCamelSource, err := cSources.Create(ctx, camelSource, meta.CreateOptions{}); err != nil {
 		c.T.Fatalf("Failed to create CamelSource %q: %v", camelSource.Name, err)
 	} else {
 		c.Tracker.AddObj(createdCamelSource)
 	}
 }
 
-func createCamelPlatformOrFail(c *testlib.Client, camelClient camelclientset.Interface, camelSourceName string) {
+func createCamelPlatformOrFail(ctx context.Context, c *testlib.Client, camelClient camelclientset.Interface, camelSourceName string) {
 	platform := camelv1.IntegrationPlatform{
 		ObjectMeta: meta.ObjectMeta{
 			Name:      "camel-k",
@@ -150,7 +154,7 @@ func createCamelPlatformOrFail(c *testlib.Client, camelClient camelclientset.Int
 		},
 	}
 
-	if _, err := camelClient.CamelV1().IntegrationPlatforms(c.Namespace).Create(&platform); err != nil {
+	if _, err := camelClient.CamelV1().IntegrationPlatforms(c.Namespace).Create(ctx, &platform, meta.CreateOptions{}); err != nil {
 		c.T.Fatalf("Failed to create IntegrationPlatform for CamelSource %q: %v", camelSourceName, err)
 	}
 }
